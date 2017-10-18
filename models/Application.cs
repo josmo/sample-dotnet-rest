114c975e11adbce7d38419110363f47aebf1a37c
Joachim Hill-Grannec <joachim.hill@gmail.com>
2017-10-18 13:07:18 -0400

    Sample REST endpoints for an applications list

---
+++ models/Application.cs
@@ -0,0 +1,13 @@
+﻿
+namespace sample_dotnet_rest.models
+{
+    public class Application
+    {
+        public int ApplicationID { get; set; }
+        public string ApplicationName { get; set; }
+        public string Description { get; set; }
+        public string ApplicationURL { get; set; }
+        public bool IsActive { get; set; }
+        
+    }
+}
---
+++ modules/Applications.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy;
+using sample_dotnet_rest.models;
+
+namespace NancyService.modules
+{
+    public class Applications : NancyModule
+    {
+        static List<String> sampleClaims = new List<string>();
+        
+        public Applications(): base("/applications")
+        {
+            
+     
+            Get("/",args =>
+            {
+                var applications = new List<Application>();
+                var application = new Application{ ApplicationID = 123, ApplicationName = "testing"};
+                applications.Add(application);
+                return Response.AsJson(applications);
+            });
+           
+            Get("/{id}", args =>
+            {
+                var application = new Application{ ApplicationID = 123, ApplicationName = "testing"};
+                return Response.AsJson(application);
+            });
+            
+            
+        }
+
+    }
+}
